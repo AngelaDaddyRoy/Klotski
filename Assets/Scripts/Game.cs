56d2c13a7bdf2d23a0e2cc762016cb15f9e12c95
roy <jnmcluo@gmail.com>
2019-02-26 00:53:15 +0800

    first commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Game : MonoBehaviour
+{
+    List<Transform> buttons;
+    void Start()
+    {
+        buttons = GameObject.Find("GameArea").GetComponentsInChildren<Transform>().OrderBy(x => x.name).ToList();
+        buttons.Remove(GameObject.Find("GameArea").transform);
+
+    }
+
+    public void StartGame()
+    {
+        var randomArray = Tools.GetRandomUnrepeatArray(1, 8, 8);
+        foreach (var item in randomArray)
+        {
+            Debug.Log(item);
+
+        }
+    }
+
+    public void ResetGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Tools
+{
+    /// <summary>
+    /// 产生随机不重复数组
+    /// </summary>
+    /// <param name="minValue"></param>
+    /// <param name="maxValue"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static int[] GetRandomUnrepeatArray(int minValue, int maxValue, int count)
+    {
+        Random rnd = new Random();
+        int length = maxValue - minValue + 1;
+        byte[] keys = new byte[length];
+        rnd.NextBytes(keys);
+        int[] items = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            items[i] = i + minValue;
+        }
+        Array.Sort(keys, items);
+        int[] result = new int[count];
+        Array.Copy(items, result, count);
+        return result;
+    }
+}
+
